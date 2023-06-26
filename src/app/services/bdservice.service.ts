1dc6babd449d655a6f8b0269ef593064c8dc787b
gvillablanca <g.villalobo.m@gmail.com>
2023-06-26 16:25:52 -0400

    cambio de estilo botones

--- src/app/services/bdservice.service.ts
+++ src/app/services/bdservice.service.ts
@@ -9,16 +9,16 @@ import { BehaviorSubject } from 'rxjs';
 export class BdserviceService {
 
   public database!: SQLiteObject;
-
   //create tables
   tablaUsuario: string = "CREATE TABLE IF NOT EXIST USUARIO( "+
-                            "ID_USER INTEGER PRIMARY KEY AUTOINCREMENT, "+
+                            "ID_USER    INTEGER PRIMARY KEY AUTOINCREMENT,"+
                             "NOMBRE		  VARCHAR(40) NOT NULL, "+
                             "APELLIDO	  VARCHAR(40) NOT NULL, "+
                             "MAIL		    VARCHAR(100) NOT NULL, "+
                             "NIVEL_EDUC	VARCHAR(100) NOT NULL,"+
                             "FECHA_NAC	DATE "+
-                            "CLAVE      VARCHAR(15) NOT NULL);"
+                            "CLAVE      VARCHAR(15) NOT NULL,"+
+                            "USER       VARCHAR(15) NOT NULL);"
 
   tablaNotas: string = "CREATE TABLE IF NOT EXIST NOTA( "+
                         "  ID_NOTA INTEGER PRIMARY KEY AUTOINCREMENT, "+
@@ -27,8 +27,7 @@ export class BdserviceService {
                         "  FOREIGN KEY (ID_USER) REFERENCES USUARIO(ID_USER));"
 
   //insert 
-  registrarUsuario: string = "INSERT OR IGNORE INTO USUARIO(NOMBRE, APELLIDO, MAIL, NIVEL_EDUC, FECHA_NAC, CLAVE) VALUES('Genoveva','villablanca', 'ge.villablanca@duocuc.cl', 'universitaria', '', '12345');"
-  
+  registrarUsuario: string = "INSERT OR IGNORE INTO USUARIO(NOMBRE, APELLIDO, MAIL, NIVEL_EDUC, FECHA_NAC, CLAVE) VALUES('Genoveva','villablanca', 'ge.villablanca@duocuc.cl', 'universitaria', '', '12345');"  
   registrarNota: string = "INSERT OR IGNORE INTO NOTA(ID_NOTA, ID_USER, DETALLE) VALUES(1,1,'ESTO ES UNA NOTA DE PRUEBA');"
 
   //observables
